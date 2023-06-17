ecdcd000fe46f5d0dd8cab3142d852ef04f48df7
robbyjm <robbymanis@tutanota.com>
2023-06-16 21:37:52 -0400

    pipe from s3

--- index.ts
+++ index.ts
@@ -1,32 +1,33 @@
-import { S3Client, GetObjectCommand, GetObjectCommandInput, GetObjectCommandOutput } from '@aws-sdk/client-s3'
-import { WriteStream, createWriteStream } from 'node:fs'
+import { S3Client, ListObjectsV2Command, ListObjectsV2CommandInput, ListObjectsV2CommandOutput, GetObjectCommand, GetObjectCommandInput, GetObjectCommandOutput } from '@aws-sdk/client-s3'
 import { Readable } from 'node:stream'
 import express, {Express, Request, Response} from 'express'
 
 const app: Express = express()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('hello')
+app.get('/', async (req: Request, res: Response) => {
+    const keys: string[] = []
+    const s3Client: S3Client = new S3Client({region: 'us-east-1'})
+    const input: ListObjectsV2CommandInput = {
+        Bucket: 'robbys-bucket'
+    }
+    const command: ListObjectsV2Command = new ListObjectsV2Command(input)
+    const response: ListObjectsV2CommandOutput = await s3Client.send(command)
+    if (response.Contents)
+        response.Contents.forEach(element => keys.push(element.Key!))
+    res.send(keys)
+})
+
+app.get('/:objectID', async (req: Request, res: Response) => {
+    const objectID: string = req.params.objectID
+    const s3Client: S3Client = new S3Client({region: 'us-east-1'})
+    const input: GetObjectCommandInput = {
+        Bucket: 'robbys-bucket',
+        Key: objectID
+    }
+    const command: GetObjectCommand = new GetObjectCommand(input)
+    const response: GetObjectCommandOutput = await s3Client.send(command)
+    const readStream = response.Body as Readable
+    readStream.pipe(res)
 })
 
 app.listen(3000)
-
-const s3Client = new S3Client({region: 'us-east-1'})
-const input: GetObjectCommandInput = {
-    Bucket: 'robbys-bucket',
-    Key: 'miragea.mp4'
-}
-const command: GetObjectCommand = new GetObjectCommand(input)
-try {
-    const response: GetObjectCommandOutput = await s3Client.send(command)
-    const myStream: Readable = response.Body as Readable
-    // how to save to local file:
-    //const writable: WriteStream = createWriteStream('file.mp4')
-    //myStream.pipe(writable)
-
-} catch (error) {
-    console.log('error')
-}
-finally {
-    console.log('finally')
-}
